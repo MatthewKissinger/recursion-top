61994d89ec499e771c7969b5e5b1873d7c95f7f4
Matt <matthew.t.kissinger@gmail.com>
2022-10-28 09:48:27 -0700

    update to mergeSort function

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 // 1) create a fibonacci sequence function using iteration
 
 // ex: fibs(8) -> [0, 1, 1, 2, 3, 5, 8, 13]
@@ -16,7 +15,6 @@ function fibs(num) {
             array.push(array[i - 1] + array[i - 2]);
         }
     }
-
     return array;
 }
 
@@ -24,28 +22,47 @@ function fibs(num) {
 
 // 2) create a fibonacci sequence function using recursion
 
-// function fibsRecursive(num) {
-
-//     if (num === 0) {
-//         return [0];
-//     } else if (num === 1) {
-//         return [0, 1];
-//     }
-
-//     let array = fibsRecursive(num - 1);
-//     return [...array, array[num - 1] + array[num - 2]];
-// }
-
 function fibsRecursive (n) {
-    if (n === 1) {
-        return [0];   
-    }
-    if (n === 2) {
-        return [0, 1];
-    }
-
-    let array = fibsRecursive(n - 1);
-    array.push(array[n - 2] + array[n - 3]);
-    return array;
+    if (n < 2) {
+        return n;
+    } else {
+        return fibsRecursive(n - 1) + fibsRecursive(n -2);
+    };
 };
-console.log(fibsRecursive(8));
+// console.log(fibsRecursive(8));
+
+// Merge Sort Function
+// sorts an array from lowest to highest number given that the numbers are greater than zero
+
+// Recursive Function
+
+function mergeSort(array) {
+    if (array.length <= 2) {
+        // sort array[0] and array [1] with a comparison operator
+        let a = array[0];
+        let b = array[1];
+        let c = [];
+
+        if (a < b) {
+            c[0] = a;
+            c[1] = b;
+        } else {
+            c[0] = b;
+            c[1] = a;
+        }
+
+        console.log(c);
+
+        return c;
+    } else {
+        // splitting array in half
+        let half = Math.ceil(array.length / 2);
+        let firstHalf = array.slice(0, half);
+        let secondHalf = array.slice(half);
+
+        // sorting each array
+        return mergeSort(firstHalf).concat(mergeSort(secondHalf));
+    }
+}
+
+console.log(mergeSort([5, 1, 2, 3]));
